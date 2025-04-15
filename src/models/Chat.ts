7d7b4cc9cf115c30ee6df8cacc4750e75d786bf1
Nicolas <nicolasuliana10@gmail.com>
2025-04-15 01:35:21 -0300

    Model Chat

---
+++ src/models/Chat.ts
@@ -0,0 +1,13 @@
+import { model, Schema } from "mongoose";
+
+const chatSchema = new Schema(
+    {
+        participants: {
+            client_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+            employed_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        },
+    },
+    { timestamps: true }
+);
+
+export const chatModel = model("Chat", chatSchema);
