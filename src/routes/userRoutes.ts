a56fa9566fa9b4e3880ab1caf82449a600d37fa1
Nicolas <nicolasuliana10@gmail.com>
2025-05-28 22:24:52 -0300

    feat(userRoutes): add validations, middlewares, and secure user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,20 +1,27 @@
-import {Router} from "express"
+import { Router } from "express"
 
 // Controllers
-import { createUsers } from "../controller/userController"
+import { checkuser, createUsers, editUser, getUserById, loginUser, myProfile } from "../controller/userController"
+
+// Middlewares
+import verifyToken from "../middleware/verify-token"
+
+// Validations
+import { userCreateValidation, userEditValidation, userLoginValidation } from "../middleware/userValidations"
+import { imageUpload } from "../middleware/image-uploader"
 
 const router = Router()
 
-router.post("/register", createUsers /* verfyToken, */ /*createUser */ )
-router.post("/login", /* verfyToken, */ /*loginUser */ )
+router.post("/register", userCreateValidation(), createUsers)
+router.post("/login", userLoginValidation(), loginUser)
 
-router.get("/checkuser", /* checkuser */ )
+router.get("/checkuser", checkuser)
 
-router.patch("/edit", /* verfyToken, imageUpload, editUser */)
+router.patch("/edit", verifyToken, imageUpload.single("image"), userEditValidation(), editUser)
 
-router.get("/myprofile", /* verifyToken, myProfile */)
+router.get("/myprofile", verifyToken, myProfile)
 
-router.get("/:id", /* getUserById */ )
+router.get("/:id", verifyToken, getUserById)
 
 
 
