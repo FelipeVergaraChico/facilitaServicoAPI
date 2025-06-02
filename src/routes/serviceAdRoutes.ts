bd3defa8f4f5f1975bb810d9a1f183feedac9ee7
Nicolas <nicolasuliana10@gmail.com>
2025-06-01 23:34:59 -0300

    refactor: organize ServiceAd routes and integrate validations

--- src/routes/serviceAdRoutes.ts
+++ src/routes/serviceAdRoutes.ts
@@ -1,13 +1,25 @@
 import { Router } from "express"
 
 // Controllers
+import { createServiceAd, deleteServiceAd, editServiceAd, getAllServiceAd, getServiceAdById, getServiceAdByUser } from "../controller/serviceAdController"
 
 // Middlewares
+import verifyToken from "../middleware/verify-token"
 
 // Validates
+import { validate } from "../middleware/handleValidation"
+import { createServiceAdValidation } from "../middleware/serviceAdValidations"
 
 const router = Router()
 
-router.get("/", /* get */)
+router.post("/", verifyToken, createServiceAdValidation(), validate, createServiceAd)
+
+router.get("/user/:userId", getServiceAdByUser)
+router.get("/", getAllServiceAd)
+router.get("/:id", getServiceAdById)
+
+router.patch("/:id", verifyToken, /* validate */ editServiceAd)
+
+router.delete("/:id", verifyToken, deleteServiceAd)
 
 export default router
