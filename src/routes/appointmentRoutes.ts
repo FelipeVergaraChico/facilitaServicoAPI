477a439dd902e1f3a681ef12534794705f1ab0c4
Nicolas <nicolasuliana10@gmail.com>
2025-06-03 16:55:01 -0300

    feat: setup base route file for Appointment

---
+++ src/routes/appointmentRoutes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express"
+
+// Controllers
+
+
+// Middlewares
+import verifyToken from "../middleware/verify-token"
+
+// Validates
+
+
+const router = Router()
+
+router.get("/", /* Started */)
+
+
+export default router
