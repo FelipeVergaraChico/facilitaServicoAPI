97bec6903ba87b2a93db7c0b5d6ff8fa2c5e457c
Nicolas <nicolasuliana10@gmail.com>
2025-06-03 22:36:52 -0300

    Add receiverId field to Message model to identify message recipients

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -4,7 +4,8 @@ const messageSchema = new Schema(
     {
         chatId: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
         senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
-        message: { type: String, required: true },
+        receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        message: { type: String, required: true }
     },
     { timestamps: true }
 );
